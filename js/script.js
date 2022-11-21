44c406a9fa19afc3f9e3511eb47d8ed65eeb7048
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-21 16:22:30 +0100

    js train ticket calculator

--- js/script.js
+++ js/script.js
@@ -5,3 +5,18 @@
 //    - va applicato uno sconto del 20% per i minorenni
 //    - va applicato uno sconto del 40% per gli over 65.
 // L'output del prezzo finale va emesso (presentato all'utente) in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+const chilometri= prompt('Ciao! Quanti chilometri vuoi percorrere?')
+const eta= prompt('Quanti anni hai?')
+
+let prezzo = 0.21 * chilometri;
+
+if (eta < 18) {
+    let scontoVenti = prezzo *20 / 100;
+    document.getElementById('prezzo').innerHTML = 'Il costo del biglietto è: '+scontoVenti;
+} else if (eta > 65) {
+    let scontoQuaranta = prezzo *40 / 100;
+    document.getElementById('prezzo').innerHTML = 'Il costo del biglietto è: ' +scontoQuaranta;
+} else{
+    document.getElementById('prezzo').innerHTML = 'Il costo del biglietto è: ' +prezzo;
+}
